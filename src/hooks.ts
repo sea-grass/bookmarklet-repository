038011eae0fa64638234f644b6e34da8dca747f5
sea-grass <dev.seagrass@gmail.com>
2021-12-09 23:36:48 -0500

    Minify html

---
+++ src/hooks.ts
@@ -0,0 +1,22 @@
+import { minify } from 'html-minifier';
+
+export async function handle({ request, resolve }) {
+	const response = await resolve(request);
+
+	if (response.headers['content-type'] === 'text/html') {
+		const body = minify(response.body, {
+			collapseWhitespace: true,
+			collapseBooleanAttributes: true,
+			removeRedundantAttributes: true,
+			useShortDoctype: true,
+			removeComments: true
+		});
+		console.log('minifying!', body);
+		return {
+			...response,
+			body
+		};
+	}
+
+	return response;
+}
