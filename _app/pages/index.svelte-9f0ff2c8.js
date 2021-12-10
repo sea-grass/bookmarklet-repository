7b201a27467d87e89c0cc3a4295f697f190b743f
sea-grass <sea-grass@users.noreply.github.com>
2021-12-10 04:18:51 +0000

    deploy: fad5ade108920b06627b8073af52b308f7147050

--- _app/pages/index.svelte-9f0ff2c8.js
+++
@@ -1,3 +0,0 @@
-import{S as d,i as k,s as u,e as c,t as f,c as l,a as p,g as v,d as i,f as _,G as m,O as n}from"../chunks/vendor-689b80eb.js";function x(h){let e,t,s;return{c(){e=c("main"),t=c("p"),s=f(`The Bookmarklet Repository is a website containing JavaScript Bookmarklets, which are small
-		snippets of JavaScript code that are executed whenever you click its bookmark!`)},l(a){e=l(a,"MAIN",{});var o=p(e);t=l(o,"P",{});var r=p(t);s=v(r,`The Bookmarklet Repository is a website containing JavaScript Bookmarklets, which are small
-		snippets of JavaScript code that are executed whenever you click its bookmark!`),r.forEach(i),o.forEach(i)},m(a,o){_(a,e,o),m(e,t),m(t,s)},p:n,i:n,o:n,d(a){a&&i(e)}}}class w extends d{constructor(e){super();k(this,e,null,x,u,{})}}export{w as default};
